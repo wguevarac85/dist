f8124286ba2699f66e2f9e20ae5dc3ab0d6adcfc
wguevarac85 <wguevarac85@gmail.com>
2023-11-10 01:58:30 -0500

    Creacion Menu

--- js/scripts-min.js
+++ js/scripts-min.js
@@ -1,27 +1,43 @@
 "use strict";
 
-console.log('eso es un archivo');
-var sliderContainer = document.getElementById('slider-container');
-var slider = document.getElementById('slider');
-var buttonLeft = document.getElementById('button-left');
-var buttonRight = document.getElementById('button-right');
-var rootStyles = document.documentElement.style;
-console.log(rootStyles);
-var DIRECTION = {
-  RIGHT: 'RIGHT',
-  LEFT: 'LEFT'
-};
-var moveSlide = function moveSlide(direction) {
-  if (direction === DIRECTION.LEFT) {
-    rootStyles.setProperty('--slide-transform', '300px');
-  } else if (direction == DIRECTION.RIGHT) {
-    rootStyles.setProperty('--slide-transform', '-300px');
+(function () {
+  var listElements = document.querySelectorAll('.menu__item--show');
+  var list = document.querySelector('.menu__links');
+  var menu = document.querySelector('.menu__hamburguer');
+  var addClick = function addClick() {
+    listElements.forEach(function (element) {
+      element.addEventListener('click', function () {
+        var subMenu = element.children[1];
+        var height = 0;
+        element.classList.toggle('menu__item--active');
+        if (subMenu.clientHeight === 0) {
+          height = subMenu.scrollHeight;
+        }
+        subMenu.style.height = "".concat(height, "px");
+      });
+    });
+  };
+  var deleteStyleHeight = function deleteStyleHeight() {
+    listElements.forEach(function (element) {
+      if (element.children[1].getAttribute('style')) {
+        element.children[1].removeAttribute('style');
+        element.classList.remove('menu__item--active');
+      }
+    });
+  };
+  window.addEventListener('resize', function () {
+    if (window.innerWidth > 800) {
+      deleteStyleHeight();
+      if (list.classList.contains('menu__links--show')) list.classList.remove('menu__links--show');
+    } else {
+      addClick();
+    }
+  });
+  if (window.innerWidth <= 800) {
+    addClick();
   }
-};
-buttonRight.addEventListener('click', function () {
-  return moveSlide(DIRECTION.RIGHT);
-});
-buttonLeft.addEventListener('click', function () {
-  return moveSlide(DIRECTION.LEFT);
-});
+  menu.addEventListener('click', function () {
+    return list.classList.toggle('menu__links--show');
+  });
+})();
 //# sourceMappingURL=scripts-min.js.map
